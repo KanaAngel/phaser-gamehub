6897b40c98b311bb4bfa3c1acf49ab45e2bd02d4
Morgan Hofmann <morgan@6L-LTINF02.atlas.atlascollege.nl>
2021-05-18 12:27:43 +0200

    World generation

--- rpg/index.js
+++ rpg/index.js
@@ -1,20 +1,57 @@
 var config = {
   type: Phaser.AUTO,
   width: 800,
-  height: 450,
+  height: 480,
   parent: 'gameContainer',
   scene: {
     preload: preload,
-    create: create
+    create: create,
+    update: update
+  },
+  render: {
+    antialias: false,
+    antialiasGL: false,
+    pixelArt: true,
+    roundPixels: true,
+    clearBeforeRender: true,
+    mipmapFilter: 'NEAREST'
   }
 }
 
 var game = new Phaser.Game(config);
+var scene;
 
 function preload() {
   console.log("preload()");
+  var texture = this.load.spritesheet('terrain', 'assets/sprites/terrain.png', { frameWidth: 16, frameHeight: 16 });
+  console.log(texture);
+
+  scene = this;
 }
 
 function create() {
-  console.log("create()");
+  newTerrain();
+}
+
+function newTerrain(threshold = 0.5) {
+  console.log("newTerrain()");
+  for (var i = 0; i < 25; i++)
+    for (var j = 0; j < 15; j++) {
+      if (Math.random() > threshold) createTerrainAt(i, j);
+      else createWaterAt(i, j);
+    }
+}
+
+function createTerrainAt(x, y) {
+  var sprite = scene.add.image((x*32)+16, (y*32)+16, 'terrain', 0);
+  sprite.scale = 2;
+}
+
+function createWaterAt(x, y) {
+  var sprite = scene.add.image((x*32)+16, (y*32)+16, 'terrain', 1);
+  sprite.scale = 2;
+}
+
+function update() {
+
 }
