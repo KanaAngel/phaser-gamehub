c72a66883c965edde5aa3d4feeb86ae902ffbe00
Morgan Hofmann <morgan@6L-LTINF02.atlas.atlascollege.nl>
2021-05-18 13:03:41 +0200

    Start terrain rendering based on neighbouring terrain.

--- rpg/index.js
+++ rpg/index.js
@@ -21,34 +21,75 @@ var config = {
 var game = new Phaser.Game(config);
 var scene;
 
+var worldSize = 25;
+
+var world = [];
+
 function preload() {
   console.log("preload()");
-  var texture = this.load.spritesheet('terrain', 'https://raw.githubusercontent.com/KanaAngel/phaser-gamehub/main/rpg/assets/sprites/terrain.png', { frameWidth: 16, frameHeight: 16 });
+  var texture = this.load.spritesheet('terrain', 'assets/sprites/terrain.png', { frameWidth: 16, frameHeight: 16 });
   console.log(texture);
 
   scene = this;
 }
 
 function create() {
+  worldObj();
   newTerrain();
+}
+
+function worldObj() {
+  world = new Array(worldSize);
+  for (var i = 0; i < worldSize; i++) {
+    world[i] = new Array(worldSize);
+    for (var j = 0; j < worldSize; j++) {
+      world[i][j] = 0;
+    }
+  }
 }
 
 function newTerrain(threshold = 0.5) {
   console.log("newTerrain()");
-  for (var i = 0; i < 25; i++)
-    for (var j = 0; j < 15; j++) {
-      if (Math.random() > threshold) createTerrainAt(i, j);
+
+  for (var i = 0; i < worldSize; i++)
+    for (var j = 0; j < worldSize; j++) {
+      if (Math.random() > threshold) world[i][j] = 0; // 1 = land
+      else world[i][j] = 1; // 0 = water
+    }
+
+  renderTerrain();
+}
+
+function renderTerrain() {
+  console.log("renderTerrain()");
+
+  for (var i = 0; i < worldSize; i++)
+    for (var j = 0; j < worldSize; j++) {
+      if (world[i][j] == 1) createTerrainAt(i, j);
       else createWaterAt(i, j);
     }
 }
 
 function createTerrainAt(x, y) {
-  var sprite = scene.add.image((x*32)+16, (y*32)+16, 'terrain', 0);
+  var spriteIndex = 0;
+
+  if (x < 1 || y < 1 || x > worldSize - 1 || y > worldSize - 1) {
+    spriteIndex = 6;
+  } else {
+    var left = world[x - 1][y] == 1;
+    var top = world[x][y + 1] == 1;
+    var right = world[x + 1][y] == 1;
+    var bottom = world[x][y - 1] == 1;
+
+    console.log(spriteIndex);
+  }
+
+  var sprite = scene.add.image((x*32)+16, (y*32)+16, 'terrain', spriteIndex);
   sprite.scale = 2;
 }
 
 function createWaterAt(x, y) {
-  var sprite = scene.add.image((x*32)+16, (y*32)+16, 'terrain', 1);
+  var sprite = scene.add.image((x*32)+16, (y*32)+16, 'terrain', 0);
   sprite.scale = 2;
 }
 
