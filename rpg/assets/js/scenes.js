931416d0cda20067372555df77e1331491a99ea0
Morgan Hofmann <morgan@6L-LTINF02.atlas.atlascollege.nl>
2021-06-07 09:31:20 +0200

    Start scene integration.

---
+++ rpg/assets/js/scenes.js
@@ -0,0 +1,85 @@
+var space;
+var mainScene = new Phaser.Class({
+  initialize: function() {
+    console.log("Loaded main scene.");
+  },
+  preload: function() {
+    this.load.spritesheet('terrain', 'assets/sprites/terrain.png', { frameWidth: 16, frameHeight: 16 });
+
+  },
+  update: function () {
+    worldObj();
+    newTerrain();
+
+    addInputEvents();
+  },
+  create: mainCreate
+});
+
+function worldObj() {
+  world = new Array(worldSize);
+  for (var i = 0; i < worldSize; i++) {
+    world[i] = new Array(worldSize);
+    for (var j = 0; j < worldSize; j++) {
+      world[i][j] = 0;
+    }
+  }
+}
+
+function newTerrain(threshold = 0.5) {
+  for (var i = 0; i < worldSize; i++)
+    for (var j = 0; j < worldSize; j++) {
+      if (Math.random() > threshold) world[i][j] = 0; // 1 = land
+      else world[i][j] = 1; // 0 = water
+    }
+
+  renderTerrain();
+}
+
+function renderTerrain() {
+  for (var i = 0; i < worldSize; i++)
+    for (var j = 0; j < worldSize; j++) {
+      if (world[i][j] == 1) createTerrainAt(i, j);
+      else createWaterAt(i, j);
+    }
+}
+
+function createTerrainAt(x, y) {
+  var spriteIndex = 0;
+
+  var left = x == 0 || world[x - 1][y] == 1;
+  var top = y == 0 || world[x][y - 1] == 1;
+  var right = x == worldSize - 1 || world[x + 1][y] == 1;
+  var bottom = y == worldSize - 1 || world[x][y + 1] == 1;
+
+  var combinedValue = left + top + right + bottom;
+
+  if (!left && !top && !right && !bottom) spriteIndex = 1;
+  if (!left && top && !right && !bottom) spriteIndex = 2;
+  if (left && !top && !right && !bottom) spriteIndex = 3;
+  if (!left && !top && right && !bottom) spriteIndex = 4;
+  if (!left && !top && !right && bottom) spriteIndex = 5;
+  if (combinedValue > 2 || (top && bottom) || (left && right)) spriteIndex = 6;
+  if (left && top && !right && !bottom) spriteIndex = 7;
+  if (!left && top && right && !bottom) spriteIndex = 8;
+  if (!left && !top && right && bottom) spriteIndex = 9;
+  if (left && !top && !right && bottom) spriteIndex = 10;
+
+  if (spriteIndex == 0) spriteIndex = 1;
+
+  var sprite = scene.add.image((x*32)+16, (y*32)+16, 'terrain', spriteIndex);
+  sprite.scale = 2;
+}
+
+function createWaterAt(x, y) {
+  var sprite = scene.add.image((x*32)+16, (y*32)+16, 'terrain', 0);
+  sprite.scale = 2;
+}
+
+function addInputEvents() {
+  var spaceInput = this.input.keyboard.addKey(Phaser.Input.Keyboard.KeyCodes.SPACE);
+}
+
+function mainUpdate() {
+
+}
--- rpg/index.js
+++ rpg/index.js
@@ -3,11 +3,7 @@ var config = {
   width: 800,
   height: 480,
   parent: 'gameContainer',
-  scene: {
-    preload: preload,
-    create: create,
-    update: update
-  },
+  scene: [mainScene],
   render: {
     antialias: false,
     antialiasGL: false,
@@ -19,82 +15,8 @@ var config = {
 }
 
 var game = new Phaser.Game(config);
-var scene;
+Phaser.Scene.call(this, mainScene);
 
 var worldSize = 25;
 
 var world = [];
-
-function preload() {
-  var texture = this.load.spritesheet('terrain', 'assets/sprites/terrain.png', { frameWidth: 16, frameHeight: 16 });
-  scene = this;
-}
-
-function create() {
-  worldObj();
-  newTerrain();
-}
-
-function worldObj() {
-  world = new Array(worldSize);
-  for (var i = 0; i < worldSize; i++) {
-    world[i] = new Array(worldSize);
-    for (var j = 0; j < worldSize; j++) {
-      world[i][j] = 0;
-    }
-  }
-}
-
-function newTerrain(threshold = 0.5) {
-  for (var i = 0; i < worldSize; i++)
-    for (var j = 0; j < worldSize; j++) {
-      if (Math.random() > threshold) world[i][j] = 0; // 1 = land
-      else world[i][j] = 1; // 0 = water
-    }
-
-  renderTerrain();
-}
-
-function renderTerrain() {
-  for (var i = 0; i < worldSize; i++)
-    for (var j = 0; j < worldSize; j++) {
-      if (world[i][j] == 1) createTerrainAt(i, j);
-      else createWaterAt(i, j);
-    }
-}
-
-function createTerrainAt(x, y) {
-  var spriteIndex = 0;
-
-  var left = x == 0 || world[x - 1][y] == 1;
-  var top = y == 0 || world[x][y - 1] == 1;
-  var right = x == worldSize - 1 || world[x + 1][y] == 1;
-  var bottom = y == worldSize - 1 || world[x][y + 1] == 1;
-
-  var combinedValue = left + top + right + bottom;
-
-  if (!left && !top && !right && !bottom) spriteIndex = 1;
-  if (!left && top && !right && !bottom) spriteIndex = 2;
-  if (left && !top && !right && !bottom) spriteIndex = 3;
-  if (!left && !top && right && !bottom) spriteIndex = 4;
-  if (!left && !top && !right && bottom) spriteIndex = 5;
-  if (combinedValue > 2 || (top && bottom) || (left && right)) spriteIndex = 6;
-  if (left && top && !right && !bottom) spriteIndex = 7;
-  if (!left && top && right && !bottom) spriteIndex = 8;
-  if (!left && !top && right && bottom) spriteIndex = 9;
-  if (left && !top && !right && bottom) spriteIndex = 10;
-
-  if (spriteIndex == 0) spriteIndex = 1;
-
-  var sprite = scene.add.image((x*32)+16, (y*32)+16, 'terrain', spriteIndex);
-  sprite.scale = 2;
-}
-
-function createWaterAt(x, y) {
-  var sprite = scene.add.image((x*32)+16, (y*32)+16, 'terrain', 0);
-  sprite.scale = 2;
-}
-
-function update() {
-
-}
